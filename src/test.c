06dda0ca3984e45cb942be75a1bf7af00bffe5ae
Anki <dharmesh@ubuntu.ubuntu-domain>
2014-06-08 16:50:40 +0530

    Addition a and b printed

--- src/test.c
+++ src/test.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 
 int main(){
+	int a,b;
+	a = 10;
+	b = 20;
+	c = a + b;
 	printf("This is my first git program\n");
 	printf("This is all about merging exercise");
+	printf("a + b = %d",c);
 	return 0;
 }
