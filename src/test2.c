12591509e8806e7d5061b3da6f17bc8ba1ef8a38
Anki <dharmesh@ubuntu.ubuntu-domain>
2014-06-08 17:17:30 +0530

    Addition function added

--- src/test2.c
+++ src/test2.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int subtract(int,int);
+int addition(int,int);
 
 int subtract(int a, int b){
 	int c;
 	c = a-b;
+	return c;
+}
+
+int addition(int a, int b){
+	int c = 0;
+	c = a+b;
 	return c;
 }
